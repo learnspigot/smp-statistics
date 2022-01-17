54914c68af5aee4d6001db339a68c0e9cc873ba6
Bungoh <dbunnik3@gatech.edu>
2022-01-17 14:01:06 -0500

    Init Project

---
+++ src/main/java/com/bungoh/smpstatistics/SMPStatistics.java
@@ -0,0 +1,17 @@
+package com.bungoh.smpstatistics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SMPStatistics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
