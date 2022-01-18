efaa74a2668e488cbdecb8b713871d497e1b8831
Bungoh <bungohdev@gmail.com>
2022-01-17 19:52:17 -0500

    Remove onDisable method

--- src/main/java/com/bungoh/smpstatistics/SMPStatistics.java
+++ src/main/java/com/bungoh/smpstatistics/SMPStatistics.java
@@ -5,7 +5,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SMPStatistics extends JavaPlugin {
-
     @Override
     public void onEnable() {
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") == null) {
@@ -15,10 +14,5 @@ public final class SMPStatistics extends JavaPlugin {
         }
 
         new SMPStatsExpansion().register();
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
     }
 }
